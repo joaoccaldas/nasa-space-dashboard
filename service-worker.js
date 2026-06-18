efc37a474408c9a46c5b1724b45e7ed11039a157
João Caldas <joaoccaldas@gmail.com>
2026-06-18 18:27:12 +0200

    chore: refresh cache for CaldaSpace 3.1 modules

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='caldaspace-v3';
-const SHELL=['./','./index.html','./bootstrap.js','./manifest.webmanifest','./parts/c4/10.txt','./parts/c4/11.txt','./parts/a5/12.txt','./parts/a5/13.txt'];
+const CACHE='caldaspace-v3-1';
+const SHELL=['./','./index.html','./bootstrap.js','./manifest.webmanifest','./parts/c5/12.txt','./parts/a6/14.txt','./parts/a6/15.txt'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL))));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response;})));});
