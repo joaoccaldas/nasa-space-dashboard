35dc6dff6d16fd359a2e1633b15dcafe13c6eac0
João Caldas <joaoccaldas@gmail.com>
2026-06-18 17:50:21 +0200

    feat: cache Living Universe modules

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,5 @@
-const CACHE='caldaspace-v2';
-self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(['./','./index.html','./bootstrap.js','./manifest.webmanifest']))));
-self.addEventListener('fetch',event=>{if(event.request.method==='GET')event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request)));});
+const CACHE='caldaspace-v3';
+const SHELL=['./','./index.html','./bootstrap.js','./manifest.webmanifest','./parts/c4/10.txt','./parts/c4/11.txt','./parts/a5/12.txt','./parts/a5/13.txt'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL))));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response;})));});
